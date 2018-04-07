6c0ea315786baef3962e0cd593e69a543bd3dac1
Максим Ефимов <hapsidra@outlook.com>
2018-04-07 20:40:06 +0900

    main

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
-console.log("hello");
-fetch('http://codeforces.com/api/user.status?handle=hapsidra').then(function(response){
-    return response.json();
-}).then(function (problems){
-    console.log(problems);
-})
+function search(handle) {
+    var tbody = document.getElementsByTagName('table')[0].getElementsByTagName('tbody')[0];
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+    if (handle.length > 0) {
+        fetch('http://codeforces.com/api/user.status?handle=' + handle).then(function(response) {
+            return response.json();
+        }).then(function (json) {
+            var problems = json.result;
+            console.log(problems);
+            var solved = {};
+            var unsolved = {};
+            for(var i = 0;i<problems.length;i++){
+                if (problems[i].verdict === 'OK') {
+                    solved[problems[i].problem.contestId + problems[i].problem.index] = problems[i];
+                }
+            }
+            for(var i = 0;i<problems.length;i++){
+                if (solved[problems[i].problem.contestId + problems[i].problem.index] == null) {
+                    unsolved[problems[i].problem.contestId + problems[i].problem.index] = problems[i];
+                }
+            }
+
+            for (number in unsolved) {
+                var tr = document.createElement('tr');
+                var numberTd = document.createElement('td');
+                var nameTd = document.createElement('td');
+                var numberLink = document.createElement('a');
+                var nameLink = document.createElement('a');
+                numberLink.innerText = number;
+                numberLink.href = "http://codeforces.com/contest/"+unsolved[number].contestId+"/problem/"+unsolved[number].problem.index;
+                nameLink.innerText = unsolved[number].problem.name;
+                nameLink.href = "http://codeforces.com/contest/"+unsolved[number].contestId+"/problem/"+unsolved[number].problem.index;
+                numberTd.appendChild(numberLink);
+                nameTd.appendChild(nameLink);
+                tr.appendChild(numberTd);
+                tr.appendChild(nameTd);
+                tbody.appendChild(tr);
+            }
+        })
+    }
+}
