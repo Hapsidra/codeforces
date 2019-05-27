0702bf8a30455918604b70049954021a00ccaa66
hapsidra <hapsidra@gmail.com>
2019-05-28 01:32:34 +0900

    Updates

---
+++ precache-manifest.7ceb1497612a6d3efbec4ee3f570ce1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94ba5fd6b80a96aa4d9140abac64d96",
+    "url": "/codeforces-unsolved/index.html"
+  },
+  {
+    "revision": "d73ce85de38b41f80041",
+    "url": "/codeforces-unsolved/static/js/2.94f9ea89.chunk.js"
+  },
+  {
+    "revision": "ed71414f7b12681ada1e",
+    "url": "/codeforces-unsolved/static/js/main.2310d042.chunk.js"
+  },
+  {
+    "revision": "1914e4731f63b1f20ad4",
+    "url": "/codeforces-unsolved/static/js/runtime~main.0e69b1da.js"
+  }
+]);
